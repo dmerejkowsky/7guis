6cabc67a4de246ce8e1b1d127fd0ed9baebde293
Dimitri Merejkowsky <dimitri@dmerej.info>
2023-05-18 14:40:39 +0200

    Finish celcius/farenheit - start playing with WTForms

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+from flask_wtf import FlaskForm, CSRFProtect
+from wtforms import StringField, SubmitField
+
+
+class HelloForm(FlaskForm):
+    name = StringField()
+    submit = SubmitField("submit")
 
 
 def create_app():
-    app = Flask("counter")
+    app = Flask("7guis")
+    app.secret_key = "s3cr3t"
 
     _counter = 0
 
-    @app.get("/")
-    def home():
-        return "hello"
+    @app.route("/hello", methods=["GET", "POST"])
+    def hello():
+        form = HelloForm()
+        if request.method == "GET":
+            return render_template("hello.html", form=form)
+        else:
+            return render_template("hello.html", form=form)
 
     @app.get("/counter")
     def counter():
-        return render_template("counter.html", counter=_counter)
+        return render_template("counter.html")
 
     @app.post("/up")
     def on_up():
@@ -25,5 +38,47 @@ def create_app():
         nonlocal _counter
         _counter -= 1
         return str(_counter)
+
+    @app.get("/temperatures")
+    def temperatures():
+        return render_template("temperatures.html")
+
+    @app.post("/celcius")
+    def celcius():
+        try:
+            temperature_in_celcius = int(request.form.get("celcius"))
+        except ValueError:
+            temperature_in_celcius = 0
+        temperature_in_farenheit = temperature_in_celcius * (9 / 5) + 32
+        return f"""
+  <input
+    id="input-farenheit"
+    type="number"
+    hx-post="/farenheit"
+    hx-target="#input-celcius"
+    hx-swap="outerHTML"
+    name="farenheit"
+    value="{temperature_in_farenheit:.0f}"
+  >
+    """
+
+    @app.post("/farenheit")
+    def farenheit():
+        try:
+            temperature_in_farenheit = int(request.form.get("farenheit"))
+        except ValueError:
+            temperature_in_farenheit = 0
+        temperature_in_celcius = (temperature_in_farenheit - 32) * (5 / 9)
+        return f"""
+  <input
+    id="input-celcius"
+    type="number"
+    hx-post="/celcius"
+    hx-target="#input-farenheit"
+    hx-swap="outerHTML"
+    name="celcius"
+    value="{temperature_in_celcius:.0f}"
+  >
+    """
 
     return app
